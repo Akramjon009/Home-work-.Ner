6833eafcecb030aaba424d9b77fc000d8c759bb5
Akramjon <144797401+Akramjon009@users.noreply.github.com>
2024-01-17 18:55:09 +0500

    info: home work 17.01

--- PostcCreater/ControlBottn.cs
+++ PostcCreater/ControlBottn.cs
@@ -1,78 +1,72 @@
-﻿
-using Telegram.Bot.Types.ReplyMarkups;
+﻿using Telegram.Bot.Types.ReplyMarkups;
 using Telegram.Bot.Types;
 using Telegram.Bot;
 
-namespace PostcCreater
+internal class ControlBottonClass
 {
-
-    internal class ControlBottonClass
+    public static async Task StartButton(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
     {
-        public static async Task StartButton(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        ReplyKeyboardMarkup replyKeyboardMarkup = new(
+            new[]
         {
-            ReplyKeyboardMarkup replyKeyboardMarkup = new(
-                new[]
-            {
                 new KeyboardButton[] { "create post" },
-            }
-                )
-
-            {
-                ResizeKeyboard = true
-            };
-
-
-            Message sentMessage = await botClient.SendTextMessageAsync(
-                chatId: update.Message.Chat.Id,
-                text: "Yangi post yaratmoqchi bolsangiz Create postni bosing",
-                replyMarkup: replyKeyboardMarkup,
-                cancellationToken: cancellationToken);
         }
-        public static async Task CreateButton(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+            )
+
         {
-            var buttons = new List<List<KeyboardButton>>();
-
-            var buttonsgorizontal1 = new List<KeyboardButton>();
-            buttonsgorizontal1.Add(new KeyboardButton("Chanelname update"));
-            buttonsgorizontal1.Add(new KeyboardButton("PostText update"));
-            buttonsgorizontal1.Add(new KeyboardButton("Image update"));
-            buttonsgorizontal1.Add(new KeyboardButton("Link update"));
-
-            var buttonsgorizontal2 = new List<KeyboardButton>();
-            buttonsgorizontal2.Add(new KeyboardButton("back"));
-            buttonsgorizontal2.Add(new KeyboardButton("Save"));
-            buttonsgorizontal2.Add(new KeyboardButton("Send Chanel"));
-            buttons.Add(buttonsgorizontal1);
-            buttons.Add(buttonsgorizontal2);
+            ResizeKeyboard = true
+        };
 
 
-            Message sentMessage = await botClient.SendTextMessageAsync(
-                chatId: update.Message.Chat.Id,
-                text: "Choose a rpons",
-                replyMarkup: new ReplyKeyboardMarkup(buttons),
-                cancellationToken: cancellationToken);
-        }
-        public static async Task EditButtons(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
-        {
-            var buttons = new List<List<KeyboardButton>>();
-
-            var buttonsgorizontal1 = new List<KeyboardButton>();
-            buttonsgorizontal1.Add(new KeyboardButton("Edit ChanelName"));
-            buttonsgorizontal1.Add(new KeyboardButton("Edit PostText update"));
-            buttonsgorizontal1.Add(new KeyboardButton("Edit Image"));
-            buttonsgorizontal1.Add(new KeyboardButton("Edit link"));
-            var buttonsgorizontal2 = new List<KeyboardButton>();
-            buttonsgorizontal2.Add(new KeyboardButton("back"));
-            buttonsgorizontal2.Add(new KeyboardButton("Edit Save"));
-            buttons.Add(buttonsgorizontal1);
-            buttons.Add(buttonsgorizontal2);
-
-            Message sentMessage = await botClient.SendTextMessageAsync(
-                chatId: update.Message.Chat.Id,
-                text: "Choose a response",
-                replyMarkup: new ReplyKeyboardMarkup(buttons),
-                cancellationToken: cancellationToken);
-        }
+        Message sentMessage = await botClient.SendTextMessageAsync(
+            chatId: update.Message.Chat.Id,
+            text: "Yangi post yaratmoqchi bolsangiz Create postni bosing",
+            replyMarkup: replyKeyboardMarkup,
+            cancellationToken: cancellationToken);
     }
+    public static async Task CreateButton(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        var buttons = new List<List<KeyboardButton>>();
 
+        var buttonsgorizontal1 = new List<KeyboardButton>();
+        buttonsgorizontal1.Add(new KeyboardButton("ChanelName update"));
+        buttonsgorizontal1.Add(new KeyboardButton("PostText update"));
+        buttonsgorizontal1.Add(new KeyboardButton("Image update"));
+        buttonsgorizontal1.Add(new KeyboardButton("link update"));
+
+        var buttonsgorizontal2 = new List<KeyboardButton>();
+        buttonsgorizontal2.Add(new KeyboardButton("back"));
+        buttonsgorizontal2.Add(new KeyboardButton("Save"));
+        buttonsgorizontal2.Add(new KeyboardButton("Send Chanel"));
+        buttons.Add(buttonsgorizontal1);
+        buttons.Add(buttonsgorizontal2);
+
+
+        Message sentMessage = await botClient.SendTextMessageAsync(
+            chatId: update.Message.Chat.Id,
+            text: "Choose a response",
+            replyMarkup: new ReplyKeyboardMarkup(buttons),
+            cancellationToken: cancellationToken);
+    }
+    public static async Task EditButtons(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        var buttons = new List<List<KeyboardButton>>();
+
+        var buttonsgorizontal1 = new List<KeyboardButton>();
+        buttonsgorizontal1.Add(new KeyboardButton("Edit ChanelName"));
+        buttonsgorizontal1.Add(new KeyboardButton("Edit PostText update"));
+        buttonsgorizontal1.Add(new KeyboardButton("Edit Image"));
+        buttonsgorizontal1.Add(new KeyboardButton("Edit link"));
+        var buttonsgorizontal2 = new List<KeyboardButton>();
+        buttonsgorizontal2.Add(new KeyboardButton("back"));
+        buttonsgorizontal2.Add(new KeyboardButton("Edit Save"));
+        buttons.Add(buttonsgorizontal1);
+        buttons.Add(buttonsgorizontal2);
+
+        Message sentMessage = await botClient.SendTextMessageAsync(
+            chatId: update.Message.Chat.Id,
+            text: "Choose a response",
+            replyMarkup: new ReplyKeyboardMarkup(buttons),
+            cancellationToken: cancellationToken);
+    }
 }
