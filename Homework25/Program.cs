90757a77571f398192e5c5354e89aad5f5366ad2
Akramjon <144797401+Akramjon009@users.noreply.github.com>
2024-01-25 17:43:10 +0500

    info: Home work bot upd

---
+++ Homework25/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Person otherPerson = (Person)obj;
+            return Name == otherPerson.Name;
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new Person("John");
+            var b = new Person("John");
+
+            Console.WriteLine(a.Equals(b)); 
+        }
+    }
+}
