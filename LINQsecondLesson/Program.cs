1da91cc1b728f75d9f9e511b0dee9ddea37357b3
AkramjonA <akramzonabduvahobov6@gmail.com>
2024-02-20 09:26:28 +0500

    update

---
+++ LINQsecondLesson/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace LINQsecondLesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IEnumerable<string> stringList = ["a fkgfjkgfgmk a", "a", "adddddd", "ddddddda"];
+            char c = 'a';
+            var n = Task1(c, stringList);
+            foreach (var i in n) 
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public static IEnumerable<string> Task1(char c,IEnumerable<string> n) 
+        {
+
+            return n.Select(i => i.ToString()).Where(i => i.StartsWith(c) && i.EndsWith(c) && i.Length > 1);
+        } 
+    }
+}
