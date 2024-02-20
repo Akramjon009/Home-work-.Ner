5e131eaecd34cab005f947e31fc60c14a359fc44
AkramjonA <akramzonabduvahobov6@gmail.com>
2024-02-20 21:29:22 +0500

    info update

--- Home_work/Program.cs
+++ Home_work/Program.cs
@@ -4,16 +4,18 @@
     {
         static void Main(string[] args)
         {
-            List<int> numbers = new List<int> { 1, 2, 3, 4,5, 6, 7, 8, 9, 10 };
-            int i = 0;
+            #region first project 
+            //IEnumerable<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+            //int i = 0;
 
-            var res = numbers.Where(x => { i ++; return i >= 4 && i <= 6; }).ToList();
+            //var res = numbers.Where(x => { i ++; return i >= 4 && i <= 6; }).ToList();
 
-            foreach (int x in res)
-            {
-                Console.WriteLine(x);
-            }
-
+            //foreach (int x in res)
+            //{
+            //    Console.WriteLine(x);
+            //}
+            #endregion
+            
         }
     }           
 }
